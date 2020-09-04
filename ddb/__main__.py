inetum-orleans/docker-devbox-ddb

commit 0c67cf850c6e7ede41df9c766a1a46b1759ca44a

fix(shell): Fix binary shims when -h/--help is given as argument
Close #62

ddb/feature/shell/integrations.py
@@ -114,7 +114,7 @@ class BashShellIntegration(ShellIntegration):
         return True
 
     def create_binary_shim(self, shims_path: str, binary: Binary):
-        return self._write_shim(shims_path, binary.name, 'binary', "$(ddb run %s \"$@\")" % binary.name)
+        return self._write_shim(shims_path, binary.name, 'binary', "$(ddb run %s \\\"$@\\\")" % binary.name)
 
     def create_alias_binary_shim(self, shims_path: str, binary: Binary) -> Tuple[bool, str]:
         return self._write_shim(shims_path, binary.name, 'alias', binary.command())

commit 37701a61d02f9ea68c17e315f835186a93bb440b

fix(binaries): Fix conflict with --help option of ddb run command and regression introduced in 0c67cf85

ddb/__main__.py
@@ -360,7 +360,7 @@ def parse_command_line(args: Optional[Sequence[str]] = None):
 
     subparsers = opts.add_subparsers(dest="command", help='Available commands')
     for command in commands.all():
-        parser = subparsers.add_parser(command.name, help=command.description)
+        parser = command.add_parser(subparsers)
         command.configure_parser(parser)
         command_parsers[command.name] = parser
 

ddb/command/command.py
@@ -32,6 +32,12 @@ class Command(RegistryObject, ABC):
         Configure the argument parser.
         """
 
+    def add_parser(self, subparsers):
+        """
+        Add command parser into given subparsers.
+        """
+        return subparsers.add_parser(self.name, help=self.description)
+
 
 class DefaultCommand(DefaultRegistryObject, Command):
     """
@@ -98,6 +104,17 @@ class LifecycleCommand(DefaultCommand):
         super().execute()
         for phase in self._lifecycle:
             execute_phase(phase)
+
+
+class LifecycleWithoutHelpCommand(LifecycleCommand):
+    """
+    A LifecycleCommand that doesn't add --help option to it's parser.
+    """
+    def add_parser(self, subparsers):
+        """
+        Add command parser into given subparsers.
+        """
+        return subparsers.add_parser(self.name, help=self.description, add_help=False)
 
 
 def execute_command(command: Command):

ddb/feature/docker/binaries.py
@@ -62,7 +62,7 @@ class DockerBinary(Binary):
         :return:
         """
         if condition is not None and options is not None:
-            if simple_eval(condition, functions={}, names={'args': args}):
+            if simple_eval(condition, functions={}, names={'args': ' '.join(args)}):
                 params.extend(shlex.split(options))
         else:
             if options is not None:

ddb/feature/run/__init__.py
@@ -7,6 +7,7 @@ from ..feature import Feature
 from ..schema import FeatureSchema
 from ...action import Action
 from ...command import LifecycleCommand, Command
+from ...command.command import LifecycleWithoutHelpCommand
 from ...phase import Phase, DefaultPhase
 
 
@@ -34,8 +35,8 @@ class RunFeature(Feature):
     @property
     def commands(self) -> Iterable[Command]:
         return (
-            LifecycleCommand("run", "Display command to run project binary",
-                             "run"),
+            LifecycleWithoutHelpCommand("run", "Display command to run project binary",
+                                        "run"),
         )
 
     @property

ddb/feature/run/actions.py
@@ -34,10 +34,12 @@ class RunAction(Action):
             events.run.run(name=name)
 
     @staticmethod
-    def run(name: str):
+    def run(name: str, *args):
         """
         Execute the action
         """
+        if not args:
+            args = config.unknown_args
 
         binary = binaries.get(name)
-        print(subprocess.list2cmdline(binary.command(*config.unknown_args)))
+        print(subprocess.list2cmdline(binary.command(*args)))

ddb/feature/shell/integrations.py
@@ -114,7 +114,7 @@ class BashShellIntegration(ShellIntegration):
         return True
 
     def create_binary_shim(self, shims_path: str, binary: Binary):
-        return self._write_shim(shims_path, binary.name, 'binary', "$(ddb run %s \\\"$@\\\")" % binary.name)
+        return self._write_shim(shims_path, binary.name, 'binary', "$(ddb run %s \"$@\")" % binary.name)
 
     def create_alias_binary_shim(self, shims_path: str, binary: Binary) -> Tuple[bool, str]:
         return self._write_shim(shims_path, binary.name, 'alias', binary.command())