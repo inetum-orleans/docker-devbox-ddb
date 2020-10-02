72f008a5eaca17b52bf7945aae5a598d16134d4d
Toilal <toilal.dev@gmail.com>
2020-10-02 07:52:17 +0200

    fix(test): Fix eject tests when no cfssl container is already running

--- tests/it/test_eject_it.py
+++ tests/it/test_eject_it.py
@@ -2,47 +2,14 @@ import os
 
 import yaml
 from ddb.__main__ import main
-from tests.utilstest import expect_gitignore
+from tests.utilstest import expect_gitignore, setup_cfssl
 
 
 class TestEject:
-    def test_eject1(self, project_loader):
+    def test_eject1(self, project_loader, module_scoped_container_getter):
         project_loader("eject1")
 
-        main(["configure"])
-
-        assert os.path.exists("docker-compose.yml")
-        assert os.path.exists("docker-compose.yml.jsonnet")
-        assert expect_gitignore(".gitignore", "docker-compose.yml")
-
-        assert os.path.exists(os.path.join(".bin", "psql"))
-        assert expect_gitignore(".gitignore", ".bin/psql")
-
-        assert os.path.exists(os.path.join(".docker", "db", "Dockerfile.jinja"))
-        assert expect_gitignore(".gitignore", ".docker/db/Dockerfile")
-
-        main(["configure", "--eject"])
-
-        assert os.path.exists("docker-compose.yml")
-        assert not os.path.exists("docker-compose.yml.jsonnet")
-        assert not expect_gitignore(".gitignore", "docker-compose.yml")
-
-        assert os.path.exists(os.path.join(".bin", "psql"))
-        assert expect_gitignore(".gitignore", ".bin/psql")
-
-        assert not os.path.exists(os.path.join(".docker", "db", "Dockerfile.jinja"))
-        assert not expect_gitignore(".gitignore", ".docker/db/Dockerfile")
-        
-        with open('docker-compose.yml', 'r') as dc_file:
-            data = yaml.load(dc_file, yaml.SafeLoader)
-
-        with open('../expected/docker-compose.yml', 'r') as expected_dc_file:
-            expected_data = yaml.load(expected_dc_file, yaml.SafeLoader)
-
-        assert data == expected_data
-
-    def test_eject2(self, project_loader):
-        project_loader("eject2")
+        setup_cfssl(module_scoped_container_getter)
 
         main(["configure"])
 
@@ -76,8 +43,47 @@ class TestEject:
 
         assert data == expected_data
 
-    def test_eject2_with_jsonnet_disabled(self, project_loader):
+    def test_eject2(self, project_loader, module_scoped_container_getter):
         project_loader("eject2")
+
+        setup_cfssl(module_scoped_container_getter)
+
+        main(["configure"])
+
+        assert os.path.exists("docker-compose.yml")
+        assert os.path.exists("docker-compose.yml.jsonnet")
+        assert expect_gitignore(".gitignore", "docker-compose.yml")
+
+        assert os.path.exists(os.path.join(".bin", "psql"))
+        assert expect_gitignore(".gitignore", ".bin/psql")
+
+        assert os.path.exists(os.path.join(".docker", "db", "Dockerfile.jinja"))
+        assert expect_gitignore(".gitignore", ".docker/db/Dockerfile")
+
+        main(["configure", "--eject"])
+
+        assert os.path.exists("docker-compose.yml")
+        assert not os.path.exists("docker-compose.yml.jsonnet")
+        assert not expect_gitignore(".gitignore", "docker-compose.yml")
+
+        assert os.path.exists(os.path.join(".bin", "psql"))
+        assert expect_gitignore(".gitignore", ".bin/psql")
+
+        assert not os.path.exists(os.path.join(".docker", "db", "Dockerfile.jinja"))
+        assert not expect_gitignore(".gitignore", ".docker/db/Dockerfile")
+
+        with open('docker-compose.yml', 'r') as dc_file:
+            data = yaml.load(dc_file, yaml.SafeLoader)
+
+        with open('../expected/docker-compose.yml', 'r') as expected_dc_file:
+            expected_data = yaml.load(expected_dc_file, yaml.SafeLoader)
+
+        assert data == expected_data
+
+    def test_eject2_with_jsonnet_disabled(self, project_loader, module_scoped_container_getter):
+        project_loader("eject2")
+
+        setup_cfssl(module_scoped_container_getter)
 
         main(["configure"])
 
