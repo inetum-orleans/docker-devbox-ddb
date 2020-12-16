05471f2819bd0cd2bed06c07ec5d8508e8c9c0bc
Rémi Alvergnat <remi.alvergnat@gfi.world>
2020-12-16 17:13:18 +0100

    build(semantic-release): Force minor release and use forked semantic-release
    Related PR in semantic-release: https://github.com/relekang/python-semantic-release/pull/293

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ with open(requirements) as f:
     install_requires = list(map(str.strip, f.read().splitlines()))[1:]
 
 with open(requirements_dev) as f:
-    dev_require = list(filter(lambda x: not x.startswith('https://'), map(str.strip, f.read().splitlines())))[:-1]
+    dev_require = list(filter(lambda x: '://' not in x, map(str.strip, f.read().splitlines())))[:-1]
 
 dependency_link_pattern = re.compile("(\S+:\/\/\S+)#egg=(\S+)")
 
